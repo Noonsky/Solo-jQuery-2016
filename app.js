e11e76ffec975d38b05e30d6f6ebf5b7c839da08
Erik Noonan <noonan135@gmail.com>
2017-01-16 15:43:25 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var color;
+var clicks;
+
+$(document).ready(function() {
+    $('.color-button').on('click', makeBlock);
+});
+
+
+
+function makeBlock() {
+    color = $('.color-button').attr("data-color");
+    clicks = $('#' + color).text();
+    clicks = Number(clicks);
+    clicks++;
+    $('#' + color).text(clicks);
+    $('.container').append('<div class="color-cube" '+color+' "></div>');
+}
+
+//Attempt
+//   () {
+//     var color = $('button').data("color");
+//     var clicks = 0;
+//
+//     $('.container').append('<div class="color-cube" style="background-color: '+ color +' ">');
+//     $('#id'+"color").text('clicks', function(){
+//       for (i=0; i>0; i++){
+//
+//       }
+//     });
+//
+//   })
+//
+// });
+
+// function colorFinder =
+// $(this).data("color")
+
+//Attempt
+//
+// $(document).ready(function() {
+//   $('button').on('click', function() {
+//     var color = '$(this).attr("data-color")';
+//     var clicks = 0;
+//
+//     $('.container').append('<div class="color-cube" style="background-color: '+ color +' ">');
+//     $('#id'+"color").text('clicks')
+//
+//   })
+//
+// });
